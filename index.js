7a54887afaa0e8f687933d4302d9544c18bcd87d
Eric Sanchez <ericfsod@gmail.com>
2021-07-01 13:13:02 -0400

    commit tree 6f9fda0cce7bce0f544459578a7397aa8d52516a

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+const returnFirstTwoDrivers = function () {
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function () {
+    return drivers.slice(2, 4);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier (num1) {
+    function multiply () {
+        return num1 * 5;
+
+    }
+
+    return multiply;
+}
+
+//describe('fareDoubler()', function () {
+    // it('is a function', function () {
+    //     expect(fareDoubler).to.be.a('function');
+    //   });
+
+    const fareDoubler = function multiply(num1) {
+        return (num1 * 2)
+    }
+
+    const fareTripler = function multiply(num1) {
+        return (num1 * 3);
+    }
+
+    const selectDifferentDrivers = (drivers, driverFunctionPicker) => driverFunctionPicker(drivers.slice(0, 2));
